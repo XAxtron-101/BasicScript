ac92a141e47464046a27396c7fbdbc07c8b6fb7d
XAxtron-101 <goodboy56wb@gmail.com>
2025-07-04 18:26:16 +0800

    basicScript.js

--- basicScript.js
+++ basicScript.js
@@ -1,1 +1,12 @@
-console.log('Github Script loaded!');
+import { world, system } from "@minecraft/server";
+
+
+system.run(() => {
+  for (const player of world.getPlayers()) {
+    player.addTag("Say")
+    if (player.hasTag("Say")) {
+      player.runCommand("summon armor_stand ~~~");
+      player.removeTag("Say");
+    }
+  }
+});
